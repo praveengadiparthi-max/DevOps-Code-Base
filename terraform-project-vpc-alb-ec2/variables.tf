6674c11e5053119862d7bb66e381f25617a5b0a2
Praveen Gadiparthi <praveengadiparthi007@gmail.com>
2026-03-22 21:04:45 -0400

    Moved files to correct rename

---
+++ terraform-project-vpc-alb-ec2/variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_range_vpc" {
+  default = "10.0.0.0/16"
+}
+variable "cidr_range_public_subnet" {
+  default = "10.0.1.0/24"
+}
+variable "cidr_range_public_subnet_2" {
+  default = "10.0.2.0/24"
+}   
