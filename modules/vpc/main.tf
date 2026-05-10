a7ed3d70045890fcf9e52066d8c3d53eb36bbf1d
Praveen Gadiparthi <praveengadiparthi007@gmail.com>
2026-05-10 19:54:47 -0400

    Initial Terraform and DevOps configuration

---
+++ modules/vpc/main.tf
@@ -0,0 +1,37 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = merge(var.tags, { "Name" = "tf-mod-vpc" })
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags   = merge(var.tags, { "Name" = "tf-mod-igw" })
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = element(var.availability_zones, count.index)
+  tags = merge(var.tags, { "Name" = "tf-mod-public-subnet-${count.index}" })
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = merge(var.tags, { "Name" = "tf-mod-public-rt" })
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
---
+++ terraform-project-vpc-alb-ec2/userdata.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Update package list
+apt-get update -y
+
+# Install Apache2
+apt-get install apache2 -y
+
+# Ensure Apache is started and enabled at boot
+systemctl start apache2
+systemctl enable apache2
+
+# Create a simple HTML page
+cat <<EOF > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Terraform EC2</title>
+</head>
+<body>
+    <h1>Terraform</h1>
+    <p>Apache server is running on this EC2 instance.</p>
+</body>
+</html>
+EOF
